a5a6bfedf9ba93e954b782d3dcdb478c145b7c83
Guilherme Rezende <guilhermebr@gmail.com>
2020-03-11 17:56:41 -0300

    add Client structure

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/dlpco/go-stone-openbank
+
+go 1.14
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/google/uuid v1.1.1
+	github.com/sirupsen/logrus v1.4.2
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+)
---
+++ openbank.go
@@ -0,0 +1,195 @@
+package openbank
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	libraryVersion    = "1.0"
+	prodAccountURL    = "https://accounts.openbank.stone.com.br"
+	sandboxAccountURL = "https://sandbox-accounts.openbank.stone.com.br"
+	prodAPIBaseURL    = "https://api.openbank.stone.com.br"
+	sandboxAPIBaseURL = "https://sandbox-api.openbank.stone.com.br"
+	userAgent         = "go-stone-openbank/" + libraryVersion
+)
+
+type Client struct {
+	client *http.Client
+	log    *logrus.Entry
+
+	AccountURL *url.URL
+	ApiBaseURL *url.URL
+
+	ClientID           string
+	ConsentRedirectURL *url.URL
+	PrivateKeyPath     string
+
+	Sandbox bool
+
+	UserAgent string
+
+	Token string
+
+	//Services used for comunicating with API
+	//Account AccountService
+}
+
+//vhttpClient *http.Client, sandbox bool, clientID, consentRedirectURL strinxg
+func NewClient(opts ...ClientOpt) *Client {
+
+	accountURL, _ := url.Parse(prodAccountURL)
+	apiURL, _ := url.Parse(prodAPIBaseURL)
+
+	c := Client{
+		client:     http.DefaultClient,
+		UserAgent:  userAgent,
+		AccountURL: accountURL,
+		ApiBaseURL: apiURL,
+	}
+
+	c.ApplyOpts(opts...)
+
+	log := logrus.New().WithFields(logrus.Fields{
+		"apiURL":     c.ApiBaseURL,
+		"accountURL": c.AccountURL,
+	})
+
+	c.log = log
+
+	//Set services
+	//c.Account = &AccountServiceOp{client: c}
+
+	return &c
+}
+
+type ClientOpt func(*Client)
+
+func WithClientID(key string) ClientOpt {
+	return func(c *Client) {
+		c.ClientID = key
+	}
+}
+
+func SetPrivateKey(path string) ClientOpt {
+	return func(c *Client) {
+		c.PrivateKeyPath = path
+	}
+}
+
+func WithHttpClient(hc http.Client) ClientOpt {
+	return func(c *Client) {
+		c.client = &hc
+	}
+}
+
+func UseSandbox() ClientOpt {
+	return func(c *Client) {
+		accountURL, _ := url.Parse(sandboxAccountURL)
+		apiURL, _ := url.Parse(sandboxAPIBaseURL)
+		c.Sandbox = true
+		c.ApiBaseURL = apiURL
+		c.AccountURL = accountURL
+	}
+}
+
+func SetUserAgent(ua string) ClientOpt {
+	return func(c *Client) {
+		c.UserAgent = fmt.Sprintf("%s %s", ua, c.UserAgent)
+	}
+}
+
+func (c *Client) ApplyOpts(opts ...ClientOpt) {
+	for _, opt := range opts {
+		opt(c)
+	}
+}
+
+type Response struct {
+	*http.Response
+}
+
+type ErrorResponse struct {
+	// HTTP response that caused this error
+	Response *http.Response
+
+	// Error message
+	Message string `json:"message"`
+
+	// RequestID returned from the API, useful to contact support.
+	RequestID string `json:"request_id"`
+}
+
+func (r *ErrorResponse) Error() string {
+	if r.RequestID != "" {
+		return fmt.Sprintf("%v %v: %d (request %q) %v",
+			r.Response.Request.Method, r.Response.Request.URL, r.Response.StatusCode, r.RequestID, r.Message)
+	}
+	return fmt.Sprintf("%v %v: %d %v",
+		r.Response.Request.Method, r.Response.Request.URL, r.Response.StatusCode, r.Message)
+}
+
+func CheckResponse(r *http.Response) error {
+	if c := r.StatusCode; c >= 200 && c <= 299 {
+		return nil
+	}
+
+	errorResponse := &ErrorResponse{Response: r}
+	data, err := ioutil.ReadAll(r.Body)
+	if err == nil && len(data) > 0 {
+		err := json.Unmarshal(data, errorResponse)
+		if err != nil {
+			errorResponse.Message = string(data)
+		}
+	}
+
+	return errorResponse
+}
+
+func (c *Client) Do(req *http.Request, v interface{}) (*Response, error) {
+	debug := true
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		if rerr := resp.Body.Close(); err == nil {
+			err = rerr
+		}
+	}()
+	if debug {
+		dr, _ := httputil.DumpResponse(resp, true)
+		c.log.Infof("<<< RESULT:\n%s", string(dr))
+	}
+
+	response := &Response{Response: resp}
+
+	err = CheckResponse(resp)
+	if err != nil {
+		return response, err
+	}
+
+	if v != nil {
+		if w, ok := v.(io.Writer); ok {
+			_, err = io.Copy(w, resp.Body)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			err = json.NewDecoder(resp.Body).Decode(v)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	return response, err
+}
