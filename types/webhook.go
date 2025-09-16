fc277d2e84eb6208e32691319aa945313f049d9c
Charllyson Souza <charllyson.souza@hotmail.com>
2025-09-16 17:16:39 -0300

    feat: updates dependencies and go version

--- types/webhook.go
+++ types/webhook.go
@@ -1,6 +1,8 @@
 package types
 
-import "gopkg.in/square/go-jose.v2"
+import (
+	"github.com/go-jose/go-jose/v4"
+)
 
 // StonePublicKeys holds JWK keys by kid (RFC 7517)
 type StonePublicKeys map[string]*jose.JSONWebKey
